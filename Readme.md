317c05f7f4e80ea87761197078b8aecfa0b2123e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to customize the pivot grid appearance by overriding theme resources
+
+
+<p>The following example demonstrates how to override colors defined by the current theme.<br />
+In this example, we have created a black-and-white pivot grid theme from the predefined Office 2007 Black theme. To do this, we have overridden theme resources that define colors of various pivot grid elements.</p><br />
+
+
+<br/>
+
+
---
+++ VB/DXPivotGrid_OverrideThemeResources/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports DXPivotGrid_OverrideThemeResources.DataSet1TableAdapters
+
+Namespace DXPivotGrid_OverrideThemeResources
+	Partial Public Class MainWindow
+		Inherits Window
+		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
+		Public Sub New()
+			InitializeComponent()
+			pivotGridControl1.DataSource = salesPersonDataAdapter.GetData()
+		End Sub
+	End Class
+End Namespace
