e0eeb3dba291837e961d3fe7f26019555fdb7884
DevExpressExampleBot <fakeEmail>
2021-09-02 18:37:09 +0300

    autogenerate VB

--- VB/DXPivotGrid_OverrideThemeResources/MainWindow.xaml.vb
+++ VB/DXPivotGrid_OverrideThemeResources/MainWindow.xaml.vb
@@ -1,10 +1,10 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Windows
+﻿Imports System.Windows
 Imports DXPivotGrid_OverrideThemeResources.DataSet1TableAdapters
 
 Namespace DXPivotGrid_OverrideThemeResources
 	Partial Public Class MainWindow
 		Inherits Window
+
 		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
 		Public Sub New()
 			InitializeComponent()
