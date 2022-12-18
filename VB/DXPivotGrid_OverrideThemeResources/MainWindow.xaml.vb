93292da59e5116ffc1159ceac8cb983fe6bc4e4f
DevExpressExampleBot <fakeEmail>
2022-12-18 18:11:23 +0400

    Source auto update [skip ci]

--- VB/DXPivotGrid_OverrideThemeResources/MainWindow.xaml.vb
+++ VB/DXPivotGrid_OverrideThemeResources/MainWindow.xaml.vb
@@ -1,14 +1,16 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 Imports DXPivotGrid_OverrideThemeResources.DataSet1TableAdapters
 
 Namespace DXPivotGrid_OverrideThemeResources
-	Partial Public Class MainWindow
-		Inherits Window
-		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
-		Public Sub New()
-			InitializeComponent()
-			pivotGridControl1.DataSource = salesPersonDataAdapter.GetData()
-		End Sub
-	End Class
+
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Private salesPersonDataAdapter As SalesPersonTableAdapter = New SalesPersonTableAdapter()
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.pivotGridControl1.DataSource = salesPersonDataAdapter.GetData()
+        End Sub
+    End Class
 End Namespace
